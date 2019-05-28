333b0350ce7254f3de66a38390455d0e53ef75a3
jcx-1020 <1285129638@qq.com>
2019-05-28 12:40:09 +0800

    约瑟夫环

--- src/com/jcx/algorithm_linkedlist/josephus/ChildNode.java
+++ src/com/jcx/algorithm_linkedlist/josephus/ChildNode.java
@@ -17,34 +17,8 @@ public class ChildNode {
      */
     ChildNode preChildNode = null;
 
-    public ChildNode() {
-    }
-
     public ChildNode(int num) {
         this.num = num;
     }
 
-    public int getNum() {
-        return num;
-    }
-
-    public void setNum(int num) {
-        this.num = num;
-    }
-
-    public ChildNode getNextChildNode() {
-        return nextChildNode;
-    }
-
-    public void setNextChildNode(ChildNode nextChildNode) {
-        this.nextChildNode = nextChildNode;
-    }
-
-    public ChildNode getPreChildNode() {
-        return preChildNode;
-    }
-
-    public void setPreChildNode(ChildNode preChildNode) {
-        this.preChildNode = preChildNode;
-    }
 }
