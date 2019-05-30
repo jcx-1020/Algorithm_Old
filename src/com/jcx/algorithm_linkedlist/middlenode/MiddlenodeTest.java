5ee3bacd3845917170955bc18a2b585f9c30cd4f
jcx-1020 <1285129638@qq.com>
2019-05-30 16:57:51 +0800

    寻找单链表中间节点

--- src/com/jcx/algorithm_linkedlist/middlenode/MiddlenodeTest.java
+++ src/com/jcx/algorithm_linkedlist/middlenode/MiddlenodeTest.java
@@ -3,7 +3,7 @@ package com.jcx.algorithm_linkedlist.middlenode;
 /**
  * 找到未知长度链表的中间节点。
  */
-public class Test {
+public class MiddlenodeTest {
     public static void main(String[] args) {
         CycLink cycLink = new CycLink();
         //生成一个未知单链表
