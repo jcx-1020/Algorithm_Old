08fb5e9b503b4848e61dc001543234df5fd90ff1
jcx-1020 <1285129638@qq.com>
2019-05-30 16:56:31 +0800

    约瑟夫环

--- src/com/jcx/algorithm_linkedlist/josephus/JosephusTest.java
+++ src/com/jcx/algorithm_linkedlist/josephus/JosephusTest.java
@@ -4,7 +4,7 @@ package com.jcx.algorithm_linkedlist.josephus;
  * 60个人围成一圆圈依次编号，从第1个人开始依次报数，每数到第3个人就将他淘汰，如此循环进行直到仅余1个人为止。
  * 问：最后一个吃剩下的人编号是多少？
  */
-public class Test {
+public class JosephusTest {
     public static void main(String[] args) {
         CycLink cycLink = new CycLink(60,1,3);
     }
