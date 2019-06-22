604709db9eaa2ed6f2f127a4f2cb812c6c88eaef
jcx-1020 <1285129638@qq.com>
2019-06-22 18:05:01 +0800

    希尔排序

---
+++ src/com/jcx/algorithm_sort/ShellSort.java
@@ -0,0 +1,31 @@
+package com.jcx.algorithm_sort;
+
+import java.util.Arrays;
+
+/**
+ * 希尔排序
+ */
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 6, 0, 3, 9, 1, 7, 4, 8};
+        int[] sort = sort(arr);
+        System.out.println(Arrays.toString(sort));
+    }
+
+    public static int[] sort(int[] arr) {
+        int x = arr.length;
+        do {
+            x = x / 3 + 1;
+            for (int i = x; i < arr.length; i++) {
+                if (arr[i] < arr[i - x]) {
+                    for (int j = i ; j >= x && arr[j] < arr[j - x]; j -= x) {
+                        int temp = arr[j - x];
+                        arr[j - x] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        } while (x > 1);
+        return arr;
+    }
+}
