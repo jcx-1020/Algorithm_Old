4b64ed570badf824107ee461e6588e04725f0d80
jcx-1020 <1285129638@qq.com>
2019-06-22 17:21:18 +0800

    冒泡排序

---
+++ src/com/jcx/algorithm_sort/BubbleSort.java
@@ -0,0 +1,27 @@
+package com.jcx.algorithm_sort;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 9, 21, 1};
+        int[] sort = sort(arr);
+        System.out.println(Arrays.toString(sort));
+    }
+
+    public static int[] sort(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
