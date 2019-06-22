bb06d4cc39331dbb19edda5a6a6acc0902260b01
jcx-1020 <1285129638@qq.com>
2019-06-22 17:14:32 +0800

    选择排序

---
+++ src/com/jcx/algorithm_sort/SelectionSort.java
@@ -0,0 +1,27 @@
+package com.jcx.algorithm_sort;
+
+import java.util.Arrays;
+
+/**
+ * 选择排序
+ */
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 9, 21, 1};
+        int[] sort = sort(arr);
+        System.out.println(Arrays.toString(sort));
+    }
+
+    public static int[] sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
