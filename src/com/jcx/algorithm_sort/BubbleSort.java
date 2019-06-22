cbed5925664871c9f240d241956a359a5d936d81
jcx-1020 <1285129638@qq.com>
2019-06-22 18:12:15 +0800

    冒泡排序

--- src/com/jcx/algorithm_sort/BubbleSort.java
+++ src/com/jcx/algorithm_sort/BubbleSort.java
@@ -13,9 +13,12 @@ public class BubbleSort {
     }
 
     public static int[] sort(int[] arr) {
-        for (int i = arr.length - 1; i > 0; i--) {
+        boolean flag = false;
+        for (int i = arr.length - 1; i > 0 && !flag; i--) {
+            flag = true;
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
+                    flag = false;
                     int temp = arr[j + 1];
                     arr[j + 1] = arr[j];
                     arr[j] = temp;
