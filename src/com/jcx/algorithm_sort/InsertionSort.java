dca458084d9306523d07c7fef759a574f32fba4f
jcx-1020 <1285129638@qq.com>
2019-06-23 18:21:22 +0800

    插入排序

--- src/com/jcx/algorithm_sort/InsertionSort.java
+++ src/com/jcx/algorithm_sort/InsertionSort.java
@@ -8,11 +8,11 @@ import java.util.Arrays;
 public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {5, 2, 6, 0, 3, 9, 1, 7, 4, 8};
-        int[] sort = sort(arr);
-        System.out.println(Arrays.toString(sort));
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static int[] sort(int[] arr) {
+    public static void sort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] < arr[i - 1]) {
                 for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
@@ -22,6 +22,5 @@ public class InsertionSort {
                 }
             }
         }
-        return arr;
     }
 }
